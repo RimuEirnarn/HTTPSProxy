8c5cd1a21fc7e98d4c736b731431fb8c1686aea0
RimuEirnarn <rimuru720@proton.me>
2023-03-27 19:09:27 +0700

    Add CORS

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, Response, abort, render_template, request, redirect
-from future_router import Router
 from urllib.parse import SplitResult, urlsplit
 import requests
 from requests import Response as OtherResponse
 from http import HTTPStatus
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app, resources={'/': {'origins': '*'}})
 HTTPSTATUS = tuple(HTTPStatus)
+route = app
 
-route = Router()
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 def aborted(code: int, url: str):
@@ -44,10 +46,12 @@ def fetch():
 def form_response(data: OtherResponse):
     new = Response(data.content)
     new.status_code = data.status_code
+    new.content_type = data.headers['Content-Type']
     return new
 
 
 @route.get("/")
+@cross_origin(origin='*', headers=['Content-Type'])
 def get_():
     data = fetch()
     if isinstance(data, Response):
@@ -56,14 +60,13 @@ def get_():
 
 
 @route.post("/")
+@cross_origin(origin='*', headers=['Content-Type'])
 def post_():
     data = fetch()
     if isinstance(data, Response):
         return data
     return form_response(requests.post(data, data=request.form))
 
-
-route.init_app(app)
 
 if __name__ == "__main__":
     app.run()
